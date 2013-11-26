9b513ee7b13eeed13bd0bf5c7ef6c61197dd10aa
Freon26 <xexo26@yahoo.com.ph>
2013-11-25 18:27:00 -0800

    Change routes.rb=> match to get

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Spree::Core::Engine.routes.draw do
 
   namespace :blog do
-    match '/', to: 'contents#index', as: 'index'
-    match '/:slug', to: 'contents#show', as: 'content'
+    get '/', to: 'contents#index', as: 'index'
+    get '/:slug', to: 'contents#show', as: 'content'
   end
 
   namespace :api do
